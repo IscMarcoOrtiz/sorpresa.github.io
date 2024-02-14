f7bbcfc7dcf8eb3c83a2a6a76f5afe7f0b85c945
ISC Marco Ortiz <marcoantonioortizvalencia@gmail.com>
2024-02-13 16:04:20 -0800

    UPDATE 2.0

---
+++ scripts/rosa.js
@@ -0,0 +1,4 @@
+document.getElementById("continuar").addEventListener("click", function() {
+    // Redirige a la página deseada
+    window.location.href = "carta2.html";
+  });
---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+$( document ).ready(function() {
+    
+    var envelope = $('#envelope');
+    var btn_open = $("#open");
+    var btn_reset = $("#reset");
+    
+    envelope.click( function() {
+        open();
+    });
+    btn_open.click( function() {
+        open();
+    });
+    btn_reset.click( function() {
+        close();
+    });
+
+    function open() {
+        envelope.addClass("open")
+           .removeClass("close");
+    }
+    function close() {
+        envelope.addClass("close")
+           .removeClass("open");
+    }
+   
+});
+
+document.getElementById("continuar").addEventListener("click", function() {
+    // Redirige a la página deseada
+    window.location.href = "teamo.html";
+  });
